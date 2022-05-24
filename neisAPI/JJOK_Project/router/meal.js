b50302d3c7e10581989ebd456c8edd6631e622a5
over1234 <lap721181@gmail.com>
2022-05-24 15:55:06 +0900

    -2022/05/24 neis-API version 2

--- neisAPI/JJOK_Project/router/meal.js
+++ neisAPI/JJOK_Project/router/meal.js
@@ -1,8 +1,76 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 var express = require('express');
 var router = express.Router();
+const School = require('school-kr');
+const school = new School();
 
 router.get('/', (req, res) => {
-    res.sendFile(__dirname + '/template/view.html');
+    let month = req.query.month;
+    let year = req.query.year;
+    let LastDate = new Date(
+        year,
+        month,
+        0
+    )
+    let array = new Array();
+    let breakfast = new Array();
+    let lunch = new Array();
+    let dinner = new Array();
+    let meal_array = new Array();
+    const example = async function () {
+
+        // 학교 검색 및 첫 번째 결과의 학교 코드로 초기화
+        const result = await school.search(School.Region.BUSAN, '부산소프트웨어마이스터고등학교');
+        school.init(School.Type.HIGH, School.Region.BUSAN, result[0].schoolCode);
+
+        const optionMeal = await school.getMeal({
+            year: year,
+            month: month,
+            default: '급식이 없는 날',
+        });
+        for (let i = 0; i < LastDate.getDate(); i++) {
+            let key = i + 1;
+            array.push(optionMeal[key].replace(/([1-9]|1[0-9])\./gi, ""));
+        }
+        for (let i = 0; i < LastDate.getDate(); i++) {
+            if (array[i] != '급식이 없는 날') {
+                breakfast.push(array[i].substr(array[i].indexOf('[조식]'), array[i].indexOf('[중식]')))
+                if (array[i].indexOf('[석식]') != -1) // 석식이 있다면
+                {
+                    lunch.push(array[i].substring(array[i].indexOf('[중식]'), array[i].indexOf('[석식]')));
+                } else { // 없다면
+                    lunch.push(array[i].substring(array[i].indexOf('[중식]')));
+                }
+                dinner.push(array[i].substr(array[i].indexOf('[석식]')))
+            }
+            else {
+                breakfast.push(array[i]);
+                lunch.push(array[i]);
+                dinner.push(array[i]);
+            }
+        }
+
+        for (let i = 0; i < LastDate.getDate(); i++) {
+            meal_array.push({
+                'day' : [
+                    { '조식': breakfast[i] },
+                    { '중식': lunch[i] },
+                    { '석식': dinner[i] }
+                ]
+            });
+        }
+
+        const json = {
+            'School_Meal': meal_array
+        }
+        res.render('meal', { data: json, date: LastDate.getDate() })
+
+    };
+
+
+    example();
 });
+
 
 module.exports = router;
--- neisAPI/JJOK_Project/server.js
+++ neisAPI/JJOK_Project/server.js
@@ -1,6 +1,3 @@
-process.env.timeZone = "Asia/Seoul";
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
-
 const express = require('express');
 const http = require("http");
 const app = express();
@@ -15,11 +12,10 @@ const register = require('./router/register')
 const server = http.createServer(app);
 const port = 8080;
 const hostname = '127.0.0.1';
-const neis = require("neis");
-const fs = require("fs");
+const ejs = require("ejs"); 
 
 app.set('view engine', 'ejs'); 
-app.engine('html', require('ejs').renderFile);
+app.set('views', './views');
 app.use('/static', express.static('static'));
 
 app.use(express.json());
@@ -34,67 +30,9 @@ app.use('/hot_advice', hot_advice);
 app.use('/signup', signup);
 app.use('/register', register);
 
-let date = new Date();
-let month = date.getMonth() + 1;
-let year = date.getFullYear();
-let LastDate = new Date(
-    year,
-    month
-    , 0
-);
-let array = new Array();
-let meal_array = new Array();
-let sum = 0;
-
-function MakeJsonToMeal(meal_year, meal_month) {
-    const school = neis.createSchool(neis.REGION.BUSAN, "C100000450", neis.TYPE.HIGH);
-    school.getMeal(meal_year, meal_month).then(d => {
-        d.forEach(meal => {
-            day = meal.date.getDate();
-            meal_array.push({
-                [day]: [
-                    { '조식': neis.removeAllergy(meal.breakfast) },
-                    { '중식': neis.removeAllergy(meal.lunch) },
-                    { '석식': neis.removeAllergy(meal.dinner) }
-                ]
-            });
-        });
-        const json = {
-            "School_Meal":
-                meal_array
-        };
-        const JsonArray = JSON.stringify(json)
-            fs.writeFileSync(`./${meal_year}_${meal_month}_School_Meal.json`, JsonArray, 'utf8');
-        meal_array = [];
-    });
-}
-
-function MakeJsonToSchedule(year_schedule, month_schedule) {
-    neis.createSchool(neis.REGION.BUSAN, 'C100000450', neis.TYPE.HIGH).getDiary(month_schedule, year_schedule).then(list => {
-        for (let i = 1; i <= LastDate.getDate(); i++) {
-            if (list[i] != null) {
-                array.push({ "일정" : list[i] });
-            }
-            else {
-                array.push({ "일정" : '비어있음.' });
-            }
-        }
-        const json = {
-            "Schedule_Day":
-                array
-        };
-        const JsonArray = JSON.stringify(json)
-            fs.writeFileSync(`./${year_schedule}_${month_schedule}_Schedule.json`, JsonArray, 'utf8');
-        array = [];
-    });
-}
-
-MakeJsonToSchedule(year, month);
-MakeJsonToMeal(year, month);
-
 
 app.get('/', (req, res) => {
-    res.render('index.html');
+    res.render('index');
 });
 
 server.listen(port, hostname, () => {
