6796789e754e06d5d5fed183075cb8731f1b0fa1
over <80248568+over1234@users.noreply.github.com>
2022-08-26 14:38:05 +0900

    - 시간표 API 추가

---
+++ neisAPI/API_Test/TimeLine_API_Hosting/test.js
@@ -0,0 +1,28 @@
+const json = [
+    {
+        "time": "1",
+        "study": "영어"
+    },
+    {
+        "time": "2",
+        "study": "컴퓨터 네트워크"
+    },
+    {
+        "time": "3",
+        "study": "수학Ⅰ"
+    },
+    {
+        "time": "4",
+        "study": "한국사"
+    },
+    {
+        "time": "5",
+        "study": "동아리활동"
+    },
+    {
+        "time": "6",
+        "study": "동아리활동"
+    }
+]
+
+console.log(json[0].time)
